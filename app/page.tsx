f2cf4c4a6d730e6fce1a91db07758271c9d0ffcd
RichardIstvanBaba <richardwf7@gmail.com>
2026-08-10 15:12:38 +0100

    Create project structure and design systems

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
     <main>
-      <h1>Cyanotype Kilburn</h1>
+      <section>
+        <p>CYANOTYPE WORKSHOPS</p>
+
+        <h1>
+          Kilburn
+          <br />
+          in Blue
+        </h1>
+
+        <p>
+          Cyanotype workshops, artwork and creative experiments
+          in the parks of London.
+        </p>
+      </section>
     </main>
   );
 }
