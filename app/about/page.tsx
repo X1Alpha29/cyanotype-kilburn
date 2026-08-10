ecc687d32d5085d89116ef998e7bce9497f4d077
RichardIstvanBaba <richardwf7@gmail.com>
2026-08-10 17:51:36 +0100

    Prepare website for deployment

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+
+      <p>
+        Cyanotype workshops and creative events
+        in Kilburn, London.
+      </p>
+    </main>
+  );
+}
---
+++ app/gallery/page.tsx
@@ -0,0 +1,7 @@
+export default function GalleryPage() {
+  return (
+    <main>
+      {/* existing gallery */}
+    </main>
+  );
+}
